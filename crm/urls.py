ec0b2e451e1bf8ead8e4d1c2845e9b0009c8befb
Ajit Patil <compilewithajit@gmail.com>
2021-03-31 12:43:15 +0530

    Password Reset (via email) Functionality Implemented

--- crm/urls.py
+++ crm/urls.py
@@ -1,24 +1,12 @@
-"""crm URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import(
+    LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, 
+    PasswordResetConfirmView,PasswordResetCompleteView
+    )
 from leads.views import CustomUserSignupView
 
 urlpatterns = [
@@ -28,6 +16,11 @@ urlpatterns = [
     path('leads/', include('leads.urls', namespace="leads")),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     path('signup/', CustomUserSignupView.as_view(), name='signup'),
 
 ]
